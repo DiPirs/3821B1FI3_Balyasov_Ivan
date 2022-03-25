2c3bd177da898664c5147bbfb3273944488eb4ab
DiPirs <admarhanck@gmail.com>
2022-03-25 23:13:31 +0300

    np

---
+++ Audi.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Luxe.h"
+
+class Audi :public TLuxe
+{
+public:
+	std::string name = "Audi";
+	std::string description = "A German automotive company within the Volkswagen Group.";
+
+	void information();
+
+};
